6ff9b53df44287f9f6224219e509d9a55ef49323
Luis <lfmnovaes@gmail.com>
2021-11-02 19:25:55 -0300

    Remove book works

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,7 +9,8 @@ export default function Form() {
   const [category, setCategory] = useState('Romance');
   const chapter = { current: 'test', progress: '0', total: '42' };
   const dispatch = useDispatch();
-  const addNewBook = () => {
+  const addNewBook = (e) => {
+    e.preventDefault();
     const newBook = {
       id: uuidv4(),
       category,
@@ -17,11 +18,11 @@ export default function Form() {
       author,
       chapter,
     };
-    console.log(newBook);
     dispatch(addBook(newBook));
+    document.getElementById('add-new-book').reset();
   };
   return (
-    <form>
+    <form id="add-new-book">
       <h3>Add new Book:</h3>
       <input type="text" id="title" name="title" onChange={(e) => setTitle(e.target.value)} placeholder="Title" required />
       <input type="text" id="author" name="author" onChange={(e) => setAuthor(e.target.value)} placeholder="Author" required />
@@ -32,7 +33,7 @@ export default function Form() {
         <option value="Technology">Technology</option>
         <option value="Non-fiction">Non-fiction</option>
       </select>
-      <input type="button" value="Add book" onClick={addNewBook} />
+      <input type="submit" value="Add book" onClick={addNewBook} />
     </form>
   );
 }
