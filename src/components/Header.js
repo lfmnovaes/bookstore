995213e94e3ad257afe40e04e14c4e4012e21727
Luis <lfmnovaes@gmail.com>
2021-11-02 08:43:21 -0300

    Make required changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
   <nav className="navbar">
     <h1 className="navbar-brand">Bookstore CMS</h1>
     <ul className="navbar-nav">
       <li className="nav-item">
-        <NavLink to="/" activeClassName="active" exact>Books</NavLink>
+        <Link to="/" exact>Books</Link>
       </li>
       <li className="nav-item">
-        <NavLink to="/categories" activeClassName="active" exact>Categories</NavLink>
+        <Link to="/categories" exact>Categories</Link>
       </li>
     </ul>
   </nav>
