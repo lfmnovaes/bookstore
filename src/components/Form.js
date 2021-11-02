5e156cecf4f1cdedd73d6e7b11626ff31862e90e
Luis <lfmnovaes@gmail.com>
2021-11-02 18:21:10 -0300

    Add book works

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/books';
 
 export default function Form() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('Romance');
+  const chapter = { current: 'test', progress: '0', total: '42' };
+  const dispatch = useDispatch();
+  const addNewBook = () => {
+    const newBook = {
+      id: uuidv4(),
+      category,
+      title,
+      author,
+      chapter,
+    };
+    console.log(newBook);
+    dispatch(addBook(newBook));
+  };
   return (
     <form>
       <h3>Add new Book:</h3>
-      <input type="text" id="title" name="title" placeholder="Title" required />
-      <input type="text" id="author" name="author" placeholder="Author" required />
-      <select id="category" name="category">
-        <option value="romance">Romance</option>
-        <option value="mystery">Mystery</option>
-        <option value="sfiction">Science Fiction</option>
-        <option value="tech">Technology</option>
-        <option value="nfiction">Non-fiction</option>
+      <input type="text" id="title" name="title" onChange={(e) => setTitle(e.target.value)} placeholder="Title" required />
+      <input type="text" id="author" name="author" onChange={(e) => setAuthor(e.target.value)} placeholder="Author" required />
+      <select id="category" name="category" onChange={(e) => setCategory(e.target.value)}>
+        <option value="Romance">Romance</option>
+        <option value="Mystery">Mystery</option>
+        <option value="Science Fiction">Science Fiction</option>
+        <option value="Technology">Technology</option>
+        <option value="Non-fiction">Non-fiction</option>
       </select>
-      <input type="button" value="Add book" />
+      <input type="button" value="Add book" onClick={addNewBook} />
     </form>
   );
 }
