aaf683f480e8df9d2225dede48e12a526430d1be
Luis <lfmnovaes@gmail.com>
2021-11-02 22:16:29 -0300

    Make required CR changes

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -2,30 +2,7 @@ const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
 const initialState = {
-  books: [
-    {
-      id: '7a8f2ffb-862e-4d59-b47e-4b5231c81dfe',
-      category: 'Non-fiction',
-      title: 'A Short History of Nearly Everything',
-      author: 'Bill Bryson',
-      chapter: {
-        current: 'How to build a universe',
-        progress: '0',
-        total: '6',
-      },
-    },
-    {
-      id: '3fa5cecb-d4d7-4168-ba06-6db5ae9aba8c',
-      category: 'Mystery',
-      title: 'Lorem Ipsum',
-      author: 'Latin',
-      chapter: {
-        current: 'Dolor sit amet',
-        progress: '7',
-        total: '42',
-      },
-    },
-  ],
+  books: [],
 };
 
 export const addBook = (payload) => ({
