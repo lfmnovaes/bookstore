ca9dcadd9b7daf108f8ec9d23dcfd77454d9276f
Luis <lfmnovaes@gmail.com>
2021-11-04 18:35:38 -0300

    Style li links, and add borders

--- src/components/Book.js
+++ src/components/Book.js
@@ -7,15 +7,21 @@ export default function Book({ data }) {
   const dispatch = useDispatch();
   return (
     <li className="book">
-      Category:&nbsp;
-      {data.category}
-      <h3>
-        Title:&nbsp;
-        {data.title}
-      </h3>
-      <button type="button" onClick={() => dispatch(removeBook(data.id))}>
-        Remove
-      </button>
+      <div className="content">
+        <h5 className="category">
+          {data.category}
+        </h5>
+        <h3 className="title">
+          {data.title}
+        </h3>
+        <div className="links">
+          <a href="/#">Comments</a>
+          {' | '}
+          <a href="/#" onClick={() => dispatch(removeBook(data.id))}>Remove</a>
+          {' | '}
+          <a href="/#">Edit</a>
+        </div>
+      </div>
     </li>
   );
 }
