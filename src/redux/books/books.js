63df40ace2c849daad287f2a73d41a584c5e222a
Luis <lfmnovaes@gmail.com>
2021-11-04 09:34:45 -0300

    Implement Addbook to the API

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 const GET_BOOKS = 'bookStore/books/GET_BOOKS';
@@ -7,6 +5,23 @@ const API_ID = 'iQ2b9MWJlIPyFiqUfdHg';
 const API_URL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${API_ID}/books`;
 
 const initialState = [];
+
+const postBook = async (obj) => {
+  const tempBook = {
+    item_id: obj.id,
+    title: obj.title,
+    category: obj.category,
+  };
+
+  const results = await fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify(tempBook),
+  });
+  return results.status;
+};
 
 export const addBook = (payload) => ({
   type: ADD_BOOK,
@@ -20,8 +35,13 @@ export const removeBook = (payload) => ({
 
 export const getBooks = () => async (dispatch) => {
   const books = [];
-  const response = await axios.get(API_URL);
-  const [keys, values] = [Object.keys(response.data), Object.values(response.data)];
+  let keys;
+  let values;
+  await fetch(API_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      [keys, values] = [Object.keys(data), Object.values(data)];
+    });
   values.forEach((value, index) => {
     const book = {
       title: value[0].title,
@@ -39,6 +59,7 @@ export const getBooks = () => async (dispatch) => {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
+      postBook(action.payload);
       return [
         ...state,
         action.payload,
